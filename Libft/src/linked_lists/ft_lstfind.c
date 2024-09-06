8f6bdd73cf9830b16cff55f4e3721bd43c8e9e8f
Mathis Blanc <mblanc@paul-f4Br6s6.clusters.42paris.fr>
2024-09-06 22:45:11 +0200

    no leaks, pratiquement tout marche

---
+++ Libft/src/linked_lists/ft_lstfind.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstfind.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mblanc <mblanc@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 12:17:58 by mblanc            #+#    #+#             */
+/*   Updated: 2024/09/04 12:19:39 by mblanc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list  *ft_lstfind(t_list *lst, void *key)
+{
+    while (lst)
+    {
+        if (ft_memcmp(lst->content, key, ft_strlen(key)) == 0)
+            return (lst);
+        lst = lst->next;
+    }
+    return (NULL);
+}
+
---
+++ Libft/src/linked_lists/ft_lstreverse.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstreverse.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mblanc <mblanc@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 12:55:42 by mblanc            #+#    #+#             */
+/*   Updated: 2024/09/04 12:55:51 by mblanc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstreverse(t_list **lst)
+{
+    t_list *prev;
+    t_list *current;
+    t_list *next;
+
+    if (!lst || !*lst)
+        return;
+
+    prev = NULL;
+    current = *lst;
+
+    while (current)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    *lst = prev;
+}
